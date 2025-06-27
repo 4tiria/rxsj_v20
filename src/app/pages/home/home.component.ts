b2b59b5c03848be017fe857df3bb90dd62eeed67
Fedor <fedro_777@mail.ru>
2025-06-27 20:06:06 +0400

    fixed prettier problem

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,18 @@
-import { Component } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button";
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: "app-home",
-  imports: [MatButtonModule],
-  templateUrl: "./home.component.html",
-  styleUrl: "./home.component.scss",
+    selector: 'app-home',
+    imports: [MatButtonModule],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  sunRises(): void {
-    document.body.classList.toggle("darkMode", true);
-  }
+    sunRises(): void {
+        document.body.classList.toggle('darkMode', true);
+    }
 
-  sunSets(): void {
-    document.body.classList.toggle("darkMode", false);
-  }
+    sunSets(): void {
+        document.body.classList.toggle('darkMode', false);
+    }
 }
